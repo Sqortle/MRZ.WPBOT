fe6e4a697a4dbc5f1fc0b0ee84dd08e4bed44397
Sqortle <mirzasincap@gmail.com>
2025-07-18 16:41:48 +0300

    finish

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.firefox.service import Service
 import smtplib
+import time
+
 
 path = '/your/gecko/driver/path'
 service = Service(executable_path=path)
 browser = webdriver.Firefox(service=service)
-browser.get('https://whatsapp.com')
+browser.get('https://web.whatsapp.com/')
 
+time.sleep(10)  # waiting for wp to open
+browser.find_element_by_xpath('Target Button').click()
 
 def send_email(message):
     to = '<YOUR EMAIL>'
@@ -22,11 +26,29 @@ def send_email(message):
     server.quit()
 
 
-try:
-    browser.find_element_by_xpath('Your Button').click()
-    browser.refresh()
-    print("Find")
-except NoSuchElementException:
-    print("Not Find")
-    message = "Hata"
-    send_email(message)
+flag_1 = True
+flag_2 = True
+flag_3 = False
+
+while True:
+    try:
+        browser.find_element_by_xpath('Your Online Button').click()  # çevrimiçi bir butondur wpde bu butona ata
+        if flag_1:
+            message = "Online"
+            send_email(message)
+            time_flag_start = time.time()
+            flag_1 = False
+            flag_2 = True
+            flag_3 = True
+
+    except NoSuchElementException:
+        flag_1 = True
+        if flag_2:
+            message = "Offline"
+            send_email(message)
+            if flag_3:
+                # noinspection PyUnboundLocalVariable
+                time_flag_finish = time.time() - time_flag_start
+                send_email(str(time_flag_finish))
+                time_flag_start = time.time()
+            flag_2 = False
